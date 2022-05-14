6f355e4c9e294487e634a79d02e77c40275226bf
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-05-14 10:41:35 +0000

    final

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // Challenge--> checking for common items in two array
+
 let arr1 = ["e", "b", "c"];
 let arr2 = ["d", "j", "f"]
+
+// Method one
+/*
+time complexity O(N+M)
+space complexity O(1)
+*/
 let map = {
 };
 function containSameItem(arr1, arr2) {
+  if(!arr1 || !arr2) return false
   for(let i=0; i<arr1.length; i++) {
     if(!map[arr1[i]]) {
        map[arr1[i]] = true
@@ -18,3 +26,4 @@ function containSameItem(arr1, arr2) {
   return false
 }
 console.log(containSameItem(arr1, arr2))
+
